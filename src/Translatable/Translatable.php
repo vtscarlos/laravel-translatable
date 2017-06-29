ac53355249c9740b88ce12b43b8ce71ff48fa0dc
Carlos <vtscarlos@gmail.com>
2017-06-29 13:16:09 +0200

    Compatibilidad con Baum

--- src/Translatable/Translatable.php
+++ src/Translatable/Translatable.php
@@ -231,31 +231,20 @@ trait Translatable
      */
     public function save(array $options = [])
     {
-        if ($this->exists) {
-            if (count($this->getDirty()) > 0) {
-                // If $this->exists and dirty, parent::save() has to return true. If not,
-                // an error has occurred. Therefore we shouldn't save the translations.
-                if (parent::save($options)) {
-                    return $this->saveTranslations();
-                }
-
-                return false;
-            } else {
-                // If $this->exists and not dirty, parent::save() skips saving and returns
-                // false. So we have to save the translations
-                if ($saved = $this->saveTranslations()) {
-                    $this->fireModelEvent('saved', false);
-                    $this->fireModelEvent('updated', false);
-                }
-
-                return $saved;
+                    if (count($this->getDirty()) > 0)
+        {
+            $tempTranslations = $this->translations;
+            if (parent::save($options))
+            {
+                $this->translations = $tempTranslations;
+                return $this->saveTranslations();
             }
-        } elseif (parent::save($options)) {
-            // We save the translations only if the instance is saved in the database.
+            return false;
+        }
+        else
+        {
             return $this->saveTranslations();
         }
-
-        return false;
     }
 
     /**
